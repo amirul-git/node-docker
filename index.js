15146509865e52a41828be82cf8e10d92a117603
Amirul <37258290+amirul-git@users.noreply.github.com>
2023-10-15 12:55:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = 3000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello world with node + docker" });
+  res.json({ message: "Hello world with node + docker + CI/CD" });
 });
 
 app.listen(port, () => console.log(`This app run on ${port}`));
